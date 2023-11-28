44300fc8be8c84cd57fdcc7dab4296e6d50449ce
GitsSaikat <35061279+GitsSaikat@users.noreply.github.com>
2023-11-29 01:36:06 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.title('Hello, World! App is under construction')
+st.title('Hello, World! App is under construction, keep waiting for the data integrity as it is going to be redefined!')
